15ca4be83fa16f5d3bff7dd9940f3592c2a5f470
joseminaya15 <jose.minayac15@gmail.com>
2018-05-22 17:29:02 -0500

    search

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -37,6 +37,10 @@
                 </div>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav navbar-right">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
                         <li><a href="#">Link</a></li>
                         <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
