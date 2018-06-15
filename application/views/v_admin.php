f9cace4bcbb94abcaaddf71eca32e2593042ed71
joseminaya15 <jose.minayac15@gmail.com>
2018-06-15 12:59:11 -0500

    sdsad

--- application/views/v_admin.php
+++ application/views/v_admin.php
@@ -10,7 +10,7 @@
     <meta name="date"                   content="January 25, 2018"/>
     <meta name="language"               content="es">
     <meta name="theme-color"            content="#000000">
-	  <title>Aruba | Meridian</title>
+    <title>Aruba | Meridian</title>
     <link rel="shortcut icon" href="<?php echo RUTA_IMG?>logo/favicon.png">
     <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>toaster/toastr.min.css?v=<?php echo time();?>">
     <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap-select/css/bootstrap-select.min.css?v=<?php echo time();?>">
